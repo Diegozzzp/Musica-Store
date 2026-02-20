e01b83bc149470c88c134f7ad51ab4952a7197bb
Diegozzzp <diegoadan.mejias@gmail.com>
2026-02-20 13:23:10 -0400

    deshaces

--- Kiwi-Music/src/components/adminProductos.jsx
+++ Kiwi-Music/src/components/adminProductos.jsx
@@ -111,7 +111,7 @@ const AdminProductos = () => {
             const token = localStorage.getItem('token');
             if (!token) throw new Error('No se proporcionó token.');
 
-            await axios.delete(`https://musica-store-production.up.railway.app/productos/${productId}`, {
+            await axios.patch(`https://musica-store-production.up.railway.app/eliminarProducto/${productId}`, {}, {
                 headers: { Authorization: `Bearer ${token}` },
             });
 
