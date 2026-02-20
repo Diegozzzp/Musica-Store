26e94525554fdcb6d1b0b4ca05c13682471f99e9
Diegozzzp <diegoadan.mejias@gmail.com>
2026-02-20 13:49:52 -0400

    puerto

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 3002;
+const port = 3002;
 const cors = require('cors');
 const path = require('path');
 
