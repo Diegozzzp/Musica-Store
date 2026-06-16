b09e300d2aa2bd04181022323a2bcc250dbb3ad0
Diego Mejias <diegoadan.mejias@gmail.com>
2026-06-16 13:01:14 -0400

    unicos

--- Backend/Modelos/producto.js
+++ Backend/Modelos/producto.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 
 const ProductosSchema = new mongoose.Schema({
-    nombre : { type: String, required: true, min : 3, max : 30, unique: true },
+    nombre : { type: String, required: true, min : 3, max : 30 },
     precio : { type: Number, required: true, min : 3, max : 1000000 },
     cantidad : { type: Number, required: true, min : 0, max : 1000000 },
     categoria: { type: mongoose.Schema.Types.ObjectId, ref: 'categorias', required: true, default: null },
