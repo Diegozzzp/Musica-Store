a7ffa1ed5323eef41854e8fe573395d5e7dbe327
Diego Mejias <diegoadan.mejias@gmail.com>
2026-06-16 11:27:10 -0400

    index

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const usuarios = require('./Rutas/usuarios');
+const categoria = require('./Rutas/categoria');
+const rol = require('./Rutas/rol');
+const compra = require('./Rutas/compra');
+const producto = require('./Rutas/producto');
+const reportes = require('./Rutas/reportes');
+
 const app = express();
 const port = process.env.PORT || 3002;
 
@@ -44,18 +51,11 @@ mongoose.connect('mongodb+srv://kiwi-music-DB:kiwi-music-DB@kiwi-music.dhnkl.mon
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const usuarios = require('./Rutas/usuarios');
-const categoria = require('./Rutas/categoria');
-const rol = require('./Rutas/rol');
-const compra = require('./Rutas/compra');
-const producto = require('./Rutas/producto');
-const reportes = require('./Rutas/reportes');
-
-app.use(usuarios, categoria, rol, compra, producto, reportes);
-
 app.get('/', (req, res) => {
   res.json({ msg: 'API Kiwi Music funcionando' });
 });
+
+app.use(usuarios, categoria, rol, compra, producto, reportes);
 
 if (!process.env.VERCEL) {
   app.listen(port, () => {
