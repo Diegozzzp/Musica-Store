c7d68b0518fda0d68ffabba895453f3b989852d5
Diego Mejias <diegoadan.mejias@gmail.com>
2026-06-22 14:52:03 -0400

    IDS

--- Kiwi-Music/src/Pages/homePage.jsx
+++ Kiwi-Music/src/Pages/homePage.jsx
@@ -6,6 +6,7 @@ import MusicPlayer from '../components/produccMusic';
 
 const ALBUM_CATEGORY_IDS = ['6a317d004023a329c9a8b7ca', '6a317d594023a329c9a8b7d6'];
 const MERCH_CATEGORY_ID = '6a317d0f4023a329c9a8b7d2';
+const PROXIMAMENTE_CATEGORY_ID = '6a397890901b012d781e9b46';
 
 const HomePage = () => {
   return (
@@ -13,7 +14,7 @@ const HomePage = () => {
       <Banner />
       <Component categoriaIds={ALBUM_CATEGORY_IDS} titulo="Albums" />
       <Component categoriaId={MERCH_CATEGORY_ID} titulo="Merch" random />
-      <Component esProximamente titulo="Proximamente" random />
+      <Component categoriaId={PROXIMAMENTE_CATEGORY_ID} titulo="Proximamente" random />
       <MusicPlayer />
     </div>
   );
